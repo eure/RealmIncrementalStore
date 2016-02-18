f37d5dbdd27ea6e4bcd9acb967a3e69faf51272d
John Estropia <rommel.estropia@gmail.com>
2016-02-18 16:14:03 +0900

    Initial Commit

---
+++ RealmIncrementalStore/RealmIncrementalStore.h
@@ -0,0 +1,19 @@
+//
+//  RealmIncrementalStore.h
+//  RealmIncrementalStore
+//
+//  Created by John Estropia on 2016/02/18.
+//  Copyright © 2016年 John Estropia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmIncrementalStore.
+FOUNDATION_EXPORT double RealmIncrementalStoreVersionNumber;
+
+//! Project version string for RealmIncrementalStore.
+FOUNDATION_EXPORT const unsigned char RealmIncrementalStoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmIncrementalStore/PublicHeader.h>
+
+
